a1b0d0f22226edcf96451080579ab237ab8641cc
mohanasrijyesta24-byte <mohanasri.jyesta24@sasi.ac.in>
2026-07-13 14:41:10 +0530

    Update MongoDB connection string and add DNS servers

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const dns = require("dns")
+dns.setServer(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/ContactManager");
+        await mongoose.connect("mongodb+srv://mohanasrijyesta24_db_user:mohana123@cluster0.ndiizo4.mongodb.net/mohana");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
